df595bfe4b42ac7169ad7e7943a7cec78b313fa5
Marcelo Júnior <rokermarcelo@gmail.com>
2020-06-23 17:08:06 -0300

    Adicionados comentários a respeito do Javascript

--- ola-mundo.js
+++ ola-mundo.js
@@ -1,1 +1,2 @@
+// Imprime o texto "Olá mundo" na página
 document.write("Olá mundo");
--- referencia-html.js
+++ referencia-html.js
@@ -1,1 +1,5 @@
+/*
+Pesquisa pelos elementos <button> da página,
+e modifica seu texto para "Clique-me"
+*/
 document.querySelector("button").textContent = "Clique-me";
